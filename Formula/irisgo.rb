51c32a0901451a8e7c0c52f37fd20c525c1a0d42
Peizhe Guan <peizheg@gmail.com>
2026-04-02 11:29:29 -0400

    add formula

--- Formula/irisgo.rb
+++ Formula/irisgo.rb
@@ -1,7 +1,7 @@
 class Irisgo < Formula
     desc "macOS automation agent powered by AI"
     homepage "https://github.com/peizheg/irisgo-cowork"
-    url "https://github.com/peizheg/irisgo-cowork/archive/refs/tags/beta.tar.gz"
+    url "https://github.com/peizheg/irisgo-cowork/archive/refs/tags/v0.1.0.tar.gz"
     sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     version "0.1.0"
     license "MIT"
